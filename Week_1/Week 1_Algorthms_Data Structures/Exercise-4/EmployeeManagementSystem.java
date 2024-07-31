7825a37e9b8c3b978337c7397d7d22935a40095e
Varshini P <127020401+Varshinishini@users.noreply.github.com>
2024-07-31 17:03:03 +0530

    Add files via upload

---
+++ Week_1/Week 1_Algorthms_Data Structures/Exercise-4/EmployeeManagementSystem.java
@@ -0,0 +1,46 @@
+package Employee_Management_System;
+
+public class EmployeeManagementSystem {
+    private Employee[] employees;
+    private int size;
+    
+    public EmployeeManagementSystem(int capacity) {
+        employees = new Employee[capacity];
+        size = 0;
+    }
+
+    public void addEmployee(Employee employee) {
+        if (size < employees.length) {
+            employees[size++] = employee;
+        } else {
+            System.out.println("Array is full, cannot add more employees.");
+        }
+    }
+
+    public Employee searchEmployeeById(String employeeId) {
+        for (int i = 0; i < size; i++) {
+            if (employees[i].getEmployeeId().equals(employeeId)) {
+                return employees[i];
+            }
+        }
+        return null; // Not found
+    }
+
+    public void displayEmployees() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+
+    public void deleteEmployee(String employeeId) {
+        for (int i = 0; i < size; i++) {
+            if (employees[i].getEmployeeId().equals(employeeId)) {
+                for (int j = i; j < size - 1; j++) {
+                    employees[j] = employees[j + 1];
+                }
+                employees[--size] = null;
+                return;
+            }
+        }
+    }
+}
