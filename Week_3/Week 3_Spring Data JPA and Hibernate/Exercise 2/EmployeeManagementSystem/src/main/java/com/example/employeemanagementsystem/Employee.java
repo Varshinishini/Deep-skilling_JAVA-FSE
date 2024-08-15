a44d85497d68cee4b25ec88c958fcce602ed0c5a
Varshini P <127020401+Varshinishini@users.noreply.github.com>
2024-08-15 16:03:51 +0530

    Add files via upload

---
+++ Week_3/Week 3_Spring Data JPA and Hibernate/Exercise 2/EmployeeManagementSystem/src/main/java/com/example/employeemanagementsystem/Employee.java
@@ -0,0 +1,31 @@
+package com.example.employeemanagementsystem;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+}
